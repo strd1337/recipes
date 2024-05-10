036d2d33010a5904161ede20079e68200b34540e
StrdL <97736243+strd1337@users.noreply.github.com>
2024-05-10 19:07:00 +0300

    Added necessary library for creating styled components, created login page (#3)

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,56 @@
 import "./Login.css"
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { 
+  FormControl, 
+  Paper, 
+  TextField, 
+  Button, 
+  Card, 
+  CardContent, 
+  CardActions } from '@mui/material';
 
 const Login = () => {
+  const user = { 
+    username: "", 
+    password: "",
+  };
+
+  const [username , setUsername] = useState("");
+  const [password , setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+
+    user.username = username;
+    user.password = password;
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    setUsername("");
+    setPassword("");
+  }
   return (
-    <div>Login</div>
+    <div className="loginWrapper">
+      <Paper elevation={2} className="paperWrapper">
+        <FormControl className="formWrapper">
+          <Card elevation={0}>
+            <CardContent className="cardContentWrapper">
+              <TextField type="text" id="outlined-basic" label="User name"  variant="outlined" className="textField" value={username} onChange={(e) => setUsername(e.target.value)} />
+              <TextField type="password" id="outlined-basic" label="Password" variant="outlined" className="textField" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </CardContent>
+            <CardActions classes={{ root: "justifyCenter" }}>
+              <Button variant="contained" color="primary" size="medium" onClick={handleSubmit}>Submit</Button>
+            </CardActions>
+          </Card>
+        </FormControl>
+      </Paper>
+    </div>
   )
 }
+
 
 export default Login;
