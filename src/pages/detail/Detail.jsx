905521151314c7e4d594629dc0f9626789e43729
StrdL <97736243+strd1337@users.noreply.github.com>
2024-05-12 01:00:22 +0300

    Create home page (#5)
    * set up library, created methods for fetching the data
    
    * Added a form for searching a recipe
    
    * Added a card for each recipe
    
    * Added info about card details

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,8 +1,97 @@
-import "./Detail.css"
+import "./Detail.css";
+import { useState } from "react";
+import { useLocation } from 'react-router-dom';
+import { 
+  Card, 
+  CardContent,  
+  Typography,
+  Box,
+  Divider,
+  Tab,
+} from '@mui/material';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 const Detail = () => {
+  const { state } = useLocation();
+  const { label } = state;
+
+  const foodInfo = ["Nutrients", "Ingredients"];
+
+  const [value, setValue] = useState(foodInfo[0]);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>Detail</div>
+    <div>
+      <Card elevation={0}>
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="div" sx={{marginTop: '60px'}}>
+            {label}
+          </Typography>
+          <Box sx={{ width: '100%', typography: 'h4' }}>
+            <TabContext value={value}>
+              <Box sx={{ 
+                borderBottom: 1, 
+                borderColor: 'divider', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                display: 'flex',
+                marginTop: '30px',
+              }}>
+                <TabList onChange={handleChange} aria-label="info">
+                  <Tab label={foodInfo[0]} value="1" sx={{fontSize: '18px'}} />
+                  <Tab label={foodInfo[1]} value="2" sx={{fontSize: '18px'}} />
+                </TabList>
+              </Box>
+              <TabPanel value="1">
+                <Typography gutterBottom variant="h6" component="div">
+                  {state.totalNutrients.CA.label} : {" "}
+                  {Math.round(state.totalNutrients.CA.quantity)}
+                  {state.totalNutrients.CA.unit}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                  {state.totalNutrients.CHOCDF.label} : {" "}
+                  {Math.round(state.totalNutrients.CHOCDF.quantity)}
+                  {state.totalNutrients.CHOCDF.unit}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                  {state.totalNutrients.CHOLE.label} : {" "}
+                  {Math.round(state.totalNutrients.CHOLE.quantity)}
+                  {state.totalNutrients.CHOLE.unit}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                  {state.totalNutrients.ENERC_KCAL.label} : {" "}
+                  {Math.round(state.totalNutrients.ENERC_KCAL.quantity)}
+                  {state.totalNutrients.ENERC_KCAL.unit}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                  Total weight : {state.totalWeight}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                  Calories : {Math.round(state.calories)}
+                </Typography>
+                {state.digest.slice(0, 4).map((item, index) => (
+                  <Typography gutterBottom variant="h6" key="index" component="div">
+                    {item.label} : {Math.round(item.total)}
+                  </Typography>
+                ))}
+              </TabPanel>
+              <TabPanel value="2">
+                {state.ingredientLines.map((ingredient, index) => (
+                  <Typography gutterBottom variant="h6" key="index" component="div">
+                    Step {index + 1} : {ingredient}
+                  </Typography>
+                ))}
+              </TabPanel>
+              <Divider />
+            </TabContext>
+          </Box>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,60 @@
 import "./Home.css"
+import { useState } from "react";
+import axios from "axios"
+import Header from "../../components/header/Header";
+import Card from "../../components/card/Card";
+import noRecipes from "../../assets/norecipes.jpg"
+import { 
+  Typography,
+} from '@mui/material';
 
 const Home = () => {
+  const mealTypes = [
+    "Breakfast",
+    "Lunch",
+    "Dinner",
+    "Snack",
+    "TeaTime",
+  ];
+
+  const [query, setQuery] = useState("");
+  const [selectedMeal, setSelectedMeal] = useState(mealTypes[0]);
+  const [recipes, setRecipes] = useState(null);
+
+  const appId = "564bfd9b";
+  const appKey = "14e1bdbc907e2c0368d5948ca3b4af72";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&mealType=${selectedMeal}`;
+
+  const getData = async () => {
+    try {
+      const { data } = await axios(url);
+      setRecipes(data.hits);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <Header 
+        query={query} 
+        setQuery={setQuery}
+        selectedMeal={selectedMeal}
+        setSelectedMeal={setSelectedMeal} 
+        mealTypes={mealTypes}
+        getData={getData}
+      />
+      {!recipes && <img src={noRecipes} className="noRecipesImg" alt="No recipe icon"></img>}
+      {recipes?.length === 0 && 
+        <Typography variant="h3">
+          Sorry, try another food name
+        </Typography>
+      }
+      {
+        recipes?.length > 0 && <Card recipes={recipes} />
+      }
+    </div>
   )
 }
 
