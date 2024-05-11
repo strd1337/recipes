95bcd7c12f5d4b06fdcc2f934561d95d4b1cdeff
StrdL <stas20001234567@mail.ru>
2024-05-12 02:18:12 +0300

    Added logout

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRouter = () => {
-  const isUserExist = true;
+  const user = localStorage.getItem("user");
 
   return (
     <div>
-      {isUserExist ? <Outlet /> : <Navigate to="/login" />}
+      {user ? <Outlet /> : <Navigate to="/login" />}
     </div>
   )
 }
